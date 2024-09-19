10b5fffff1eaab43459d0c059eb6f11d2df43baa
Martin Dagarin <martin.dagarin@gmail.com>
2024-09-19 22:15:16 +0200

    Fixed bruno install

--- linux-setup.sh
+++ linux-setup.sh
@@ -174,13 +174,7 @@ function12 () {
 ## Install Bruno
 # @see https://www.usebruno.com/downloads
 function13 () {
-  sudo mkdir -p /etc/apt/keyrings 
-  sudo gpg --no-default-keyring --keyring /etc/apt/keyrings/bruno.gpg --keyserver keyserver.ubuntu.com --recv-keys 9FA6017ECABE0266 
-
-  echo "deb [signed-by=/etc/apt/keyrings/bruno.gpg] http://debian.usebruno.com/ bruno stable" | sudo tee /etc/apt/sources.list.d/bruno.list 
-  
-  sudo apt update 
-  sudo apt install -y bruno=1.29.0
+  sudo snap install bruno
 }
 
 ## Install Zeal Docs
